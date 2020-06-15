07f7f57af6a8684ab34dd640b5223191da9f5ab1
leegeunhyeok <dev.ghlee@gmail.com>
2020-06-15 14:14:43 +0900

    UPDATE: Core logic
    - add inquirer module based cli flow
    - update default export

--- src/core/index.ts
+++ src/core/index.ts
@@ -1,12 +1,46 @@
+import inquirer from 'inquirer';
 import { spawn } from 'child_process';
-import { CommandResponse } from '../interfaces';
+import { CommandResponse } from '../interface';
+import { inputType } from '../enum';
+
+export const main = async () => {
+  const res = await inquirer.prompt([
+    {
+      name: inputType.SUBJECT,
+      message: 'Commit message subject'
+    },
+    {
+      name: inputType.BODY,
+      message: 'Commit message body',
+      type: 'editor'
+    }
+  ]);
+
+  const subject = res[inputType.SUBJECT];
+  const body = res[inputType.BODY];
+  const commitMessage = subject + (body ? '\n\n' + body : '');
+
+  const confirm = await inquirer.prompt([
+    {
+      name: inputType.CONFIRM,
+      message: `Commit message preview\n${commitMessage}\nConfirm?`,
+      type: 'confirm'
+    }
+  ]);
+
+  if (confirm[inputType.CONFIRM]) {
+    process.exit(0);
+  } else {
+    // TODO: Write commit message as file
+  }
+};
 
 /**
  * @param cmd Command
  * @param args Arguments array
  * @return Command execute result
  */
-export default (cmd: string, args: Array<string>): Promise<CommandResponse> => {
+export const command = (cmd: string, args: Array<string>): Promise<CommandResponse> => {
   return new Promise((resolve, reject) => {
     const c = spawn(cmd, args);
     const res = { data: '', code: -1 };
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import { main } from './core';
+
+(() => {
+  main();
+})();
